b21f856cb3b6d55f0ac8f85b42b1cc8bfff63ba8
Diego Avila <aviladiego2009@gmail.com>
2025-06-12 18:13:09 -0300

    Modified main function

--- src/main/java/com/bytegenius/server/ServerApplication.java
+++ src/main/java/com/bytegenius/server/ServerApplication.java
@@ -1,13 +1,45 @@
 package com.bytegenius.server;
 
+import static java.util.Arrays.stream;
+
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ServerApplication {
-	public static void main(String[] args) {
+
+	enum DotEnv {
+		PORT,
+		CLIENT_ORIGIN_URL,
+		OKTA_OAUTH2_ISSUER,
+		OKTA_OAUTH2_AUDIENCE
+	}
+
+	public static void main(final String[] args) {
+		dotEnvSafeCheck();
+
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	private static void dotEnvSafeCheck() {
+		final var dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		stream(DotEnv.values())
+				.map(DotEnv::name)
+				.filter(varName -> dotenv.get(varName, "").isEmpty())
+				.findFirst()
+				.ifPresent(varName -> {
+					log.error("[Fatal] Missing or empty environment variable: {}", varName);
+
+					System.exit(1);
+				});
 	}
 }
